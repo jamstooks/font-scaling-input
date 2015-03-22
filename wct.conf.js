54bf35a155e48ddab10f87094f6f6a22d8588ca5
Benjamin Stookey <ben.stookey@gmail.com>
2015-03-22 16:41:08 -0700

    added safari test, updated browsers and fixed IE bug;

--- wct.conf.js
+++ wct.conf.js
@@ -13,14 +13,21 @@ module.exports = {
           base: 'SauceLabs',
           browserName: 'chrome',
           platform: 'Windows 8.1',
-          version: '40'
+          version: '41'
         },
         {
           name: 'Firefox',
           base: 'SauceLabs',
           browserName: 'firefox',
           platform: 'Windows 8.1',
-          version: '35'
+          version: '36'
+        },
+        {
+          name: 'Safari',
+          base: 'SauceLabs',
+          browserName: 'safari',
+          platform: 'OS X 10.10',
+          version: '8.0'
         },
         {
           name: 'Internet Explorer',
