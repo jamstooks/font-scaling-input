f00854c2f71229111d10dc58ab1ed44cb358a058
Benjamin Stookey <ben.stookey@gmail.com>
2015-03-22 19:32:24 -0700

    trying default sauce browsers;

--- wct.conf.js
+++ wct.conf.js
@@ -2,41 +2,11 @@ module.exports = {
   verbose: false,
   plugins: {
     local: {
-      disabled: true,
-      browsers: ['chrome', 'firefox']
+      disabled: false,
+      browsers: ['firefox']
     },
     sauce: {
-      disabled: false,
-      browsers: [
-        {
-          name: 'Chrome',
-          base: 'SauceLabs',
-          browserName: 'chrome',
-          platform: 'Windows 8.1',
-          version: '41'
-        },
-        {
-          name: 'Firefox',
-          base: 'SauceLabs',
-          browserName: 'firefox',
-          platform: 'Windows 8.1',
-          version: '36'
-        },
-        {
-          name: 'Safari',
-          base: 'SauceLabs',
-          browserName: 'safari',
-          platform: 'OS X 10.10',
-          version: '8.0'
-        },
-        {
-          name: 'Internet Explorer',
-          base: 'SauceLabs',
-          browserName: 'internet explorer',
-          platform: 'Windows 8.1',
-          version: '11'
-        }
-      ]
+      disabled: false
     }
   },
 };
